6d10d3d47a1980cf08c100dafbdbb5611157898e
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-05-18 00:28:58 +0100

    homepage list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
+import Features from "../../components/features/Features";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
@@ -7,6 +9,11 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Features />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
     </div>
   );
 };
